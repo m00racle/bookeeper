ea1be216b4ba91f9fcc5c7c869056a3f6c3a2361
mooracle <yanuar.prakosa@gmail.com>
2023-06-07 12:45:13 +0700

    add test app for render Bookeeper App

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,9 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+// test if the app there is heading of Bookeeper App
+test('render app with title', () => {
+    render(<App />);
+    const titleElement = screen.getByText(/Bookeeper App/i);
+    expect(titleElement).toBeInTheDocument();
+});
