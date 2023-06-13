34a32809acba92850283b12dab63567133bae15e
mooracle <yanuar.prakosa@gmail.com>
2023-06-13 21:18:59 +0700

    refactor app.js to incorporate comments

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ import Register from './components/register';
 function App() {
   return (
     <>
+    {/* Header */}
     <div>
       <h1>Bookeeper App</h1>
-      </div>
+    </div>
+
+    {/* Router Configuration */}
     <Router>
       <Routes>
+        {/* Home Route */}
         <Route path="/" element={<Home />} />
+
+        {/* Register Route */}
         <Route path="/register" element={<Register />} />
+
+        {/* Fallback Route */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
