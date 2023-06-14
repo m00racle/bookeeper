bf49632450f63179b94b4428acde45aafb2beaa0
mooracle <yanuar.prakosa@gmail.com>
2023-06-14 20:27:51 +0700

    prepare welcome path for auth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/home';
 import Register from './components/register';
+import Welcome from './components/welcome';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
 
         {/* Register Route */}
         <Route path="/register" element={<Register />} />
+
+        {/* Welcome Route */}
+        <Route path="/welcome" element={<Welcome />} />
 
         {/* Fallback Route */}
         <Route path="*" element={<Navigate to="/" />} />
---
+++ src/components/welcome.js
@@ -0,0 +1,15 @@
+/* This is after the user sign in */
+
+import React from "react";
+
+function Welcome () {
+    return (
+        <>
+            <h2>
+                Welcome user
+            </h2>
+        </>
+    );
+};
+
+export default Welcome;
