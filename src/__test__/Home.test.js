dab2919626b5f629303cbc083b837c3ceac02865
mooracle <yanuar.prakosa@gmail.com>
2023-06-16 08:50:22 +0700

    revert back to working home test

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,34 +1,48 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import { initializeTestEnvironment, RulesTestEnvironment } from '@firebase/rules-unit-testing';
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+// import { initializeTestEnvironment, RulesTestEnvironment } from '@firebase/rules-unit-testing';
 import Home from '../components/home';
 
-let testEnv;
+// lets test basic for the <Home /> component
 
-beforeAll(async () => {
-    const rulesResponse = await fetch('firestore.rules');
-    const rules = await rulesResponse.text()
-    testEnv = await initializeTestEnvironment({
-        projectId: 'refract-book',
-        firestore: { rules },
-    });
-
-    // Clear the Firestore database:
-    await testEnv.clearFirestore();
-
-    // setup the Firebase SDK to use emulators
-    firebase.initializeApp({
-        projectId: 'refract-book',
-    });
-    firebase.auth().useEmulator('http://127.0.0.1:9099');
-    firebase.firestore().useEmulator('localhost', 8080);
-
+test("renders Home component", () => {
+    // renders Home?
+    render(
+        <Router>
+            <Home />
+        </Router>
+    );
+    const titleElement = screen.getByText(/Login Home/i);
+    expect(titleElement).toBeInTheDocument();
 });
 
-afterAll(async () => {
-    // clean up the firebase testing environment
-    await testEnv.cleanup();
-});
+
+// let testEnv;
+
+// beforeAll(async () => {
+//     const rulesResponse = await fetch('firestore.rules');
+//     const rules = await rulesResponse.text()
+//     testEnv = await initializeTestEnvironment({
+//         projectId: 'refract-book',
+//         firestore: { rules },
+//     });
+
+//     // Clear the Firestore database:
+//     await testEnv.clearFirestore();
+
+//     // setup the Firebase SDK to use emulators
+//     firebase.initializeApp({
+//         projectId: 'refract-book',
+//     });
+//     firebase.auth().useEmulator('http://127.0.0.1:9099');
+//     firebase.firestore().useEmulator('localhost', 8080);
+
+// });
+
+// afterAll(async () => {
+//     // clean up the firebase testing environment
+//     await testEnv.cleanup();
+// });
